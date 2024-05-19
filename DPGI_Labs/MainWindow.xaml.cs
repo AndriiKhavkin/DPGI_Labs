07c1ee46c51d8908a3f31aabfb472e653712cf7c
AndriiKhavkin <tapusti7@gmail.com>
2024-05-19 16:43:25 +0300

    Added methods for buttons

--- DPGI_Labs/MainWindow.xaml.cs
+++ DPGI_Labs/MainWindow.xaml.cs
@@ -33,8 +33,49 @@ namespace DPGI_Labs
             string startupPath = System.IO.Directory.GetCurrentDirectory();
             System.IO.File.WriteAllText($"{startupPath}\\texts\\123.txt", TextBox1.Text);
             MessageBox.Show("The file was saved!");
-
         }
+
+        private void CanExecute_OpenFile(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Execute_OpenFile(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Opening file...");
+        }
+
+        private void CanExecute_DeleteText(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !string.IsNullOrEmpty(TextBox1.Text);
+        }
+
+        private void Execute_DeleteText(object sender, ExecutedRoutedEventArgs e)
+        {
+            TextBox1.Clear();
+        }
+
+        private void CanExecute_CopyText(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !string.IsNullOrEmpty(TextBox1.Text);
+        }
+        private void Execute_CopyText(object sender, ExecutedRoutedEventArgs e)
+        {
+            Clipboard.SetText(TextBox1.Text);
+        }
+
+        private void CanExecute_PasteText(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = Clipboard.ContainsText();
+        }
+
+        private void Execute_PasteText(object sender, ExecutedRoutedEventArgs e)
+        {
+            TextBox1.Text += Clipboard.GetText();
+        }
+
+        
+
 
 
         public MainWindow()
