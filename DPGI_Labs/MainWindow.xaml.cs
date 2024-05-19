5059d001a63429b4a74d2d88223d9db965d53b8d
AndriiKhavkin <tapusti7@gmail.com>
2024-05-19 16:39:35 +0300

    Task 4: xaml and few methods for *cs

--- DPGI_Labs/MainWindow.xaml.cs
+++ DPGI_Labs/MainWindow.xaml.cs
@@ -16,6 +16,27 @@ namespace DPGI_Labs
     /// </summary>
     public partial class MainWindow : Window
     {
+        private void CanExecute_Save(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (TextBox1.Text.Trim().Length > 0)
+            {
+                e.CanExecute = true;
+            }
+            else
+            {
+                e.CanExecute = false;
+            }
+        }
+
+        private void Execute_Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            string startupPath = System.IO.Directory.GetCurrentDirectory();
+            System.IO.File.WriteAllText($"{startupPath}\\texts\\123.txt", TextBox1.Text);
+            MessageBox.Show("The file was saved!");
+
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
