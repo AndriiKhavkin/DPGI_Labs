a9ff72c435abe3430616718ab4fa619488751218
AndriiKhavkin <tapusti7@gmail.com>
2024-05-25 20:22:16 +0300

    Xaml from lab4

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿INSERT INTO Students(StudentID, FullName, [Group], Address) VALUES ('S001', 'Ivan Petrenko', 'ka-33', '123 Main St, Kyiv'), 
+('S002', 'Olena Shevchenko', 'da-22', '456 Oak Ave, Lviv'),
+('S003', 'Andrii Kravchenko', 'tr-21', '789 Pine Rd, Odessa'),
+('S004', 'Maria Ivanova', 'ti-92', '101 Maple St, Kharkiv'),
+('S005', 'Oleh Tarasov', 'ti-92', '202 Birch Ln, Dnipro'),
+('S006', 'Yulia Kovalenko', 'tr-21', '303 Cedar St, Zaporizhia'),
+('S007', 'Danil Fundament', 'ka-33', '404 Elm St, Vinnytsia'),
+('S008', 'Svitlana Romanenko', 'tr-21', '505 Spruce Ave, Poltava'),
+('S009', 'Mykola Marchenko', 'ka-33', '606 Walnut Rd, Cherkasy'),
+('S010', 'Viktor Melnyk', 'ka-33', '707 Chestnut St, Rivne');
+
+
+
+SELECT * FROM Students
