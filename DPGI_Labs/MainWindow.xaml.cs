7d0f23f22b5c1bb7b6494bcfe55fcff866d22949
AndriiKhavkin <tapusti7@gmail.com>
2024-05-26 14:11:00 +0300

    Another changes

--- DPGI_Labs/MainWindow.xaml.cs
+++ DPGI_Labs/MainWindow.xaml.cs
@@ -19,17 +19,7 @@ namespace DPGI_Labs
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-
-    //public class CurrencyContext : DbContext 
-    //{
-    //    public DbSet<CurrencyInfo> ExchangeRates { get; set; }
-
-    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //    {
-    //        optionsBuilder.UseSqlServer("Server=DESKTOP-IO5GGS0;Database=CurrencyConverterDB;Trusted_Connection=True;");
-    //    }
-    //}
-
+      
     public class CurrencyInfo
     {
         public int Id { get; set; }
@@ -43,9 +33,6 @@ namespace DPGI_Labs
         public MainWindow()
         {
             InitializeComponent();
-            //InitializeDatabase();
-            //LoadExchangeRates();
-            //InitializeCurrencyComboBox();
         }
 
         protected override void OnInitialized(EventArgs e)
@@ -106,111 +93,6 @@ namespace DPGI_Labs
             {
                 MessageBox.Show("Будь ласка, введіть коректні дані.");
             }
-        }
-
-
-
-        //private void LoadExchangeRates()
-        //{
-        //   var exchangeRates = new Dictionary<string, CurrencyInfo>();
-
-        //    using (var context = new CurrencyContext())
-        //    {
-        //        exchangeRates = context.ExchangeRates.ToDictionary(rate => rate.CurrencyCode, rate => new CurrencyInfo
-        //        {
-        //            CurrencyCode = rate.CurrencyCode,
-        //            CurrencyName = rate.CurrencyName,
-        //            Rate = rate.Rate
-        //        });
-        //    }
-
-        //    var exchangeRatesList = exchangeRates.Select(kvp => new { Key = kvp.Key, Name = $"{kvp.Key} - {kvp.Value.CurrencyName}" }).ToList();
-
-        //    CurrencyComboBox.ItemsSource = exchangeRatesList;
-        //    CurrencyComboBox.DisplayMemberPath = "Name";
-        //    CurrencyComboBox.SelectedValuePath = "Key";
-        //    CurrencyComboBox.SelectedIndex = 0;
-
-        //    foreach (var currency in exchangeRates)
-        //    {
-        //        CurrencyComboBox.Items.Add(new { Key = currency.Key, Name = $"{currency.Key} - {currency.Value.CurrencyName}" });
-        //    }
-        //    CurrencyComboBox.SelectedIndex = 0;
-        //}
-
-        //private void InitializeCurrencyComboBox()
-        //{
-        //    foreach (var currency in exchangeRates)
-        //    {
-        //        CurrencyComboBox.Items.Add(new { Key = currency.Key, Name = $"{currency.Key} - {currency.Value.Name}" });
-        //    }
-        //    CurrencyComboBox.SelectedIndex = 0;
-        //}
-
-        //private void ConvertToUahButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    try
-        //    {
-        //        double amount = double.Parse(AmountTextBox.Text);
-        //        string selectedCurrency = ((dynamic)CurrencyComboBox.SelectedItem).Key;
-
-        //        using (var context = new CurrencyContext())
-        //        {
-        //            var rate = context.ExchangeRates.Single(r => r.CurrencyCode == selectedCurrency).Rate;
-        //            double result = amount * rate;
-        //            ResultTextBlock.Text = $"{amount} {selectedCurrency} = {result} грн";
-        //        }
-        //    }
-        //    catch (Exception)
-        //    {
-        //        MessageBox.Show("Будь ласка, введіть коректні дані.");
-        //    }
-        //}
-
-        //private void ConvertFromUahButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    try
-        //    {
-        //        double amount = double.Parse(AmountTextBox.Text);
-        //        string selectedCurrency = ((dynamic)CurrencyComboBox.SelectedItem).Key;
-
-        //        using (var context = new CurrencyContext())
-        //        {
-        //            var rate = context.ExchangeRates.Single(r => r.CurrencyCode == selectedCurrency).Rate;
-        //            double result = amount / rate;
-        //            ResultTextBlock.Text = $"{amount} грн = {result} {selectedCurrency}";
-        //        }
-        //    }
-        //    catch (Exception)
-        //    {
-        //        MessageBox.Show("Будь ласка, введіть коректні дані.");
-        //    }
-        //}
-
-        //private void InitializeDatabase()
-        //{
-        //    using (var context = new CurrencyContext())
-        //    {
-        //        context.Database.EnsureCreated();
-
-        //        if (!context.ExchangeRates.Any())
-        //        {
-        //            context.ExchangeRates.AddRange(new List<CurrencyInfo>
-        //            {
-        //                new CurrencyInfo { CurrencyCode = "USD", CurrencyName = "United States Dollar", Rate = 27.50 },
-        //                new CurrencyInfo { CurrencyCode = "EUR", CurrencyName = "Euro", Rate = 32.00 },
-        //                new CurrencyInfo { CurrencyCode = "GBP", CurrencyName = "British Pound Sterling", Rate = 37.50 },
-        //                new CurrencyInfo { CurrencyCode = "JPY", CurrencyName = "Japanese Yen", Rate = 0.25 },
-        //                new CurrencyInfo { CurrencyCode = "AUD", CurrencyName = "Australian Dollar", Rate = 20.00 },
-        //                new CurrencyInfo { CurrencyCode = "CAD", CurrencyName = "Canadian Dollar", Rate = 21.00 },
-        //                new CurrencyInfo { CurrencyCode = "CHF", CurrencyName = "Swiss Franc", Rate = 29.00 },
-        //                new CurrencyInfo { CurrencyCode = "CNY", CurrencyName = "Chinese Yuan", Rate = 4.30 },
-        //                new CurrencyInfo { CurrencyCode = "SEK", CurrencyName = "Swedish Krona", Rate = 3.10 },
-        //                new CurrencyInfo { CurrencyCode = "NOK", CurrencyName = "Norwegian Krone", Rate = 3.15 }
-        //            });
-        //            context.SaveChanges();
-        //        }
-        //    }
-        //}
+        }             
     }
 }
--- SQLQueryCurrency.sql
+++ SQLQueryCurrency.sql
@@ -1,12 +1,13 @@
-﻿Select * From ExchangeRates
+﻿Use CurrencyConverterDB
+Select * From ExchangeRates
 
-INSERT INTO ExchangeRates (CurrencyCode, CurrencyName, Rate) VALUES ('USD', 'United States Dollar', 27.50);
-INSERT INTO ExchangeRates (CurrencyCode, CurrencyName, Rate) VALUES ('EUR', 'Euro', 32.00);
-INSERT INTO ExchangeRates (CurrencyCode, CurrencyName, Rate) VALUES ('GBP', 'British Pound Sterling', 37.50);
-INSERT INTO ExchangeRates (CurrencyCode, CurrencyName, Rate) VALUES ('JPY', 'Japanese Yen', 0.25);
-INSERT INTO ExchangeRates (CurrencyCode, CurrencyName, Rate) VALUES ('AUD', 'Australian Dollar', 20.00);
-INSERT INTO ExchangeRates (CurrencyCode, CurrencyName, Rate) VALUES ('CAD', 'Canadian Dollar', 21.00);
-INSERT INTO ExchangeRates (CurrencyCode, CurrencyName, Rate) VALUES ('CHF', 'Swiss Franc', 29.00);
-INSERT INTO ExchangeRates (CurrencyCode, CurrencyName, Rate) VALUES ('CNY', 'Chinese Yuan', 4.30);
-INSERT INTO ExchangeRates (CurrencyCode, CurrencyName, Rate) VALUES ('SEK', 'Swedish Krona', 3.10);
-INSERT INTO ExchangeRates (CurrencyCode, CurrencyName, Rate) VALUES ('NOK', 'Norwegian Krone', 3.15);
+INSERT INTO ExchangeRates (CurrencyCode, CurrencyName, Rate) VALUES ('USD', 'United States Dollar', 39.83),
+('EUR', 'Euro', 43.20),
+('GBP', 'British Pound Sterling', 50.69),
+('JPY', 'Japanese Yen', 0.25),
+('AUD', 'Australian Dollar', 26.43),
+('CAD', 'Canadian Dollar', 29.13),
+('CHF', 'Swiss Franc', 43.61),
+('CNY', 'Chinese Yuan', 5.49),
+('SEK', 'Swedish Krona', 3.74),
+('NOK', 'Norwegian Krone', 3.75);
